f3d90b33655b6d891178c158b8d5584491d723bd
Andreza <andrezasilva744@gmail.com>
2023-03-19 14:48:34 -0300

    Convenção de implementação de nomeclatura é importante, dentro do escopo da função n tem user.email, então o melhor foi retirar, ele também conhece os argumentos email e password

--- projects/shave-xp-cypress/cypress/support/pages/login/index.js
+++ projects/shave-xp-cypress/cypress/support/pages/login/index.js
@@ -1,13 +1,28 @@
+// ṔascalCase - padrão oficial para criar classes em JS
 class Loginpage {
+	// 	Aqui temos PageObject, temos aqui o encapsulamento
 
-	submit(email, password){
-					cy.visit('http://localhost:3000')
+	submit(email, password) { // função submit e com todos os steps para clicar no botão de 'Entrar'
+		cy.visit('/')
 
-			cy.get('input[placeholder$=email]').type(user.email)
+		cy.get('input[placeholder$=email]').type(email)
 
-			cy.get('input[placeholder*=senha]').type(user.password)
+		cy.get('input[placeholder*=senha]').type(password)
 
-			cy.contains('button', 'Entrar')
-				.click()
+		cy.contains('button', 'Entrar')
+			.click()
 	}
+
+	noticeShouldBe(message) {
+		cy.get('.notice-container')
+			.should('be.visible')
+			.find('.error p')
+			.should('have.text', message)
+
+
+	}
+
 }
+
+export default new Loginpage()
+// new é a palavra reservada que ativa a classe, e o objeto que importar já pode usa-la 
