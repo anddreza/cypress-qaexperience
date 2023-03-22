7cf905b692f71b87c45234536549c5557adf1ab3
Andreza <andrezasilva744@gmail.com>
2023-03-22 19:33:56 -0300

    Algumas coisas foram alteradas, mas nem todas, necessário haver refatoração de algumas linhas

--- projects/shave-xp-cypress/cypress/e2e/login.cy.js
+++ projects/shave-xp-cypress/cypress/e2e/login.cy.js
@@ -1,21 +1,22 @@
-import loginPage from '../support/pages/login' 
+/// <reference types="cypress" />
+import loginPage from '../support/pages/login'
 // importando a classe objeto dentro desse login page
 import shaversPage from '../support/pages/shaves'
+import header from '../support/components/header'
+import data from '../fixtures/users.json' //importando o json dentro do login.cy.js
+
 
 describe('login', () => {
 
 	context('quando submeto o formulário', () => {
 
 		it('deve logar com sucesso', () => {
-			const user = {
-				name: 'Andressa',
-				email: 'andressa@icloud.com',
-				password: 'pwd123'
-			}
-
-			loginPage.submit(user.email, user.password)
-
-			shaversPage.header.userShouldBeLoggedIn(user.name)
+			//const user = 
+// foi transferido para users.json
+			loginPage.submit(data.email, data.password)
+			//userShouldLoggenIn(user.name)
+			//loginPage.userShouldBeLoggenIn(user.name) -> é uma validação. 
+			shaversPage.header.userShouldBeLoggedIn(data.name) // chamando a validação que informa o usuário correto na página correta
 		})
 
 
@@ -26,12 +27,11 @@ describe('login', () => {
 				password: 'abc123'
 			}
 
-
 			loginPage.submit(user.email, user.password)
 
-			const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais'
-
+			const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
 			loginPage.noticeShouldBe(message)
+
 		})
 
 		it('não deve logar com email não cadastrado', () => {
@@ -41,17 +41,69 @@ describe('login', () => {
 				password: 'abc123'
 			}
 
-
 			loginPage.submit(user.email, user.password)
 
-			const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais'
-
+			const message = 'Ocorreu um erro ao fazer login, verifique suas credenciais.'
 			loginPage.noticeShouldBe(message)
 
 		})
 
-		
+		// enviando uma string vazia 
+		// cypress não entende isso 
+		it('campos obrigatórios', () =>{
+			//loginPage.submit('', '')
+			loginPage.submit()
+			loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+			// cy.get('.alert-error') ao invés de usar o get uso o contains
+			// .should('have.text', 'E-mail é obrigatório)
+			// should('be.visible')
 
+			// cy.get('.alert-error')
+			// should('have.text', 'Senha é obrigatória')
+			// should('be.visible')
+
+			
+
+			})
+		})
+ 
+	context('senha muito curta', () => {
+		const passwords = [
+			'1',
+			'12',
+			'123',
+			'1234',
+			'12345'
+		]
+
+		passwords.forEach((p) => {
+			it(`não deve logar com a senha: ${p}`, ()=> {
+				loginPage.submit('andressa@icloud.com', p)
+				loginPage.alertShouldBe('Pelo menos 6 caracteres')
+
+			})
+		})
 	})
 
+	context('email no formato incorreto', () => {
+		const emails = [
+			'andressa&icloud.com',
+			'andressa.com.br',
+			'@icloud.com',
+			'@', 
+			'andressa@',
+			'123123',
+			'!@#!@#',
+			'xpto123'
+		]
+
+		emails.forEach((e) => {
+			it(`não deve logar com a senha: ${e}`, ()=> {
+				loginPage.submit(e, 'pwd123')
+				loginPage.alertShouldBe('Informe um email válido')
+
+
+			})
+		})
+	})
 })
--- projects/shave-xp-cypress/cypress/support/pages/login/index.js
+++ projects/shave-xp-cypress/cypress/support/pages/login/index.js
@@ -1,13 +1,31 @@
 // ṔascalCase - padrão oficial para criar classes em JS
 class Loginpage {
-	// 	Aqui temos PageObject, temos aqui o encapsulamento
 
-	submit(email, password) { // função submit e com todos os steps para clicar no botão de 'Entrar'
+	// 	Aqui temos PageObject, temos aqui o encapsulamento
+	constructor(){
+		this.alertError = '.alert-error'
+	}
+	// submit email = null, password = null
+	submit(email = null, password = null) { // função submit e com todos os steps para clicar no botão de 'Entrar'
 		cy.visit('/')
 
-		cy.get('input[placeholder$=email]').type(email)
+		cy.get('input[placeholder$=email]').as('email')
+		cy.get('input[placeholder*=senha]').as('password')
 
-		cy.get('input[placeholder*=senha]').type(password)
+		if (email) {
+			cy.get('@email').type(email)
+			// agora que tenho o as('@email')
+
+		}
+		if (password) {
+			cy.get('@password').type(password)
+			// agora que tenho o as('@password)
+		}
+
+
+		//cy.get('input[placeholder$=email]').type(email)
+
+		//cy.get('input[placeholder*=senha]').type(password)
 
 		cy.contains('button', 'Entrar')
 			.click()
@@ -22,6 +40,21 @@ class Loginpage {
 
 	}
 
+	alertShouldBe(message) {
+		cy.get(this.alertError)
+			.should('be.visible')
+			.should('have.text', message)
+	}
+
+	requiredFields(emailMessage, passwordMessage) {
+		cy.get(this.alertError)
+			.should('have.length', 2)
+			.and(($small) => { // o $ é porque está buscando um elemento HTML 
+				expect($small.get(0).textContent).to.equal(emailMessage)
+				expect($small.get(1).textContent).to.equal(passwordMessage)
+
+			})
+	}
 }
 
 export default new Loginpage()
