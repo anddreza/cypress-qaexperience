ba95b619965932012e85625b026fe625df45019c
Andreza <andrezasilva744@gmail.com>
2023-03-27 12:34:30 -0300

    Ateração do bem

---
+++ projects/shave-xp-cypress/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+const { removeUser } = require('./cypress/support/tasks/index')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+          removeUser
+      })
+    },
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    baseUrl: 'http://localhost:3000'
+  },
+});
