abfbfd4345ace9e07bd7e5969307e7bc42f97c21
Andreza <andrezasilva744@gmail.com>
2023-03-30 09:01:17 -0300

    Criação da API Helper

--- projects/shave-xp-cypress/api/app.js
+++ projects/shave-xp-cypress/api/app.js
@@ -1,9 +1,30 @@
+const bcrypt = require('bcrypt')
+const Joi = require('joi')
 const express = require ('express')
+
+
+const validator = require('express-joi-validation').createValidator({
+	passError: true
+})
+
+
 const app = express ()
 
 app.use(express.json())
 
-const { deleteUser } = require('./db')
+
+const { deleteUser, insertUser } = require('./db')
+
+
+const userSchema = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().required(),
+	is_shaver: Joi.boolean().required()
+  })
+
+
+
 
 app.get('/welcome', function(req, res){
 	res.json({message: 'Olá'})
@@ -16,8 +37,47 @@ app.delete('/user/:email', async function(req, res){
 	res.status(204).end()
 })
 
-app.post('/user', async function(req, res){
+app.post('/user', validator.body(userSchema), async function(req, res){
+	console.log(req.body)
 	
+	const { name, email, password, is_shaver } = req.body
+
+	const hashPass = await bcrypt.hash(password, 8)
+
+	const user = {
+		name: name, 
+		email: email, 
+		password: hashPass,
+		is_shaver: is_shaver
+	}
+
+//	if(!user.name || !user.email || !user.password){
+//		return res.status(400).json({ message: 'Every fields is mandatory.'})
+//	}
+
+	console.log(user)
+
+	try{
+		await deleteUser(user.email)
+		const id = await insertUser(user)
+		res.status(201).json({ user_id: id })
+	} catch (error) {
+		res.status(500).json({ error: 'Ocorreu um erro. ', stack: error })
+	}	
 })
 
+app.use((err, req, res, next) => {
+	if (err && err.error && err.error.isJoi) {
+	  // we had a joi error, let's return a custom 400 json response
+	  res.status(400).json({
+		type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+		message: err.error.toString()
+	  });
+	} else {
+	  // pass on to another error handler
+	  next(err);
+	}
+  });
+
+  
 app.listen(5000)
