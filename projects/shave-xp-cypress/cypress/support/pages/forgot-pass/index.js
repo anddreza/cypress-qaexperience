389d686a2cd1cccf5a9a93cd973d3ee1cb16b6de
Andreza <andrezasilva744@gmail.com>
2023-04-04 14:30:46 -0300

    Commit nas atualizações novas, mas também teve alteração na pasta da api

---
+++ projects/shave-xp-cypress/cypress/support/pages/forgot-pass/index.js
@@ -0,0 +1,27 @@
+class ForgotPassPage {
+// função que vai acessar a pagina
+	go(){
+		cy.visit('/forgot-password')
+		
+		// checkpoint para garantir que estamos indo para o lugar certo 
+		cy.get('form h1')
+			.should('have.text', 'Recuperar senha')
+		}
+
+	// submete o formulario de solicitação de troca de senha
+	submit(){
+		cy.get('input[placeholder$=mail]')		
+			.type(email)
+
+		cy.contains('button', 'Recuperar')
+			.click()	
+	}
+
+	noticeShouldBe(expectedText){
+		cy.get('.notice p', { timeout: 10000 })
+			.should('be.visible')
+			.should('have.text', expectedText)
+	}
+}
+
+export default new ForgotPassPage()
