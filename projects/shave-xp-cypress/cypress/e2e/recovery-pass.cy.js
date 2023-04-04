82835be9f31c6c2ae0ed5fe0ca318637f6b10295
Andreza <andrezasilva744@gmail.com>
2023-04-04 15:05:15 -0300

    Fianlizando o cenário de deve poder cadastrar uma nova senha, criando um context

--- projects/shave-xp-cypress/cypress/e2e/recovery-pass.cy.js
+++ projects/shave-xp-cypress/cypress/e2e/recovery-pass.cy.js
@@ -1,5 +1,5 @@
-/// <reference types="cypress" />
-
+import fpPage from "../support/pages/forgot-pass"
+import rpPage from '../support/pages/reset-pass'
 descibre ('Esqueci minha senha',() => {
 	it('Deve poder solicitar o resgate de senha', () => {
 
@@ -13,20 +13,34 @@ descibre ('Esqueci minha senha',() => {
 		cy.createUser(user)
 
 		fpPage.go()
-		fpPage.submit(email, password)
+		fpPage.submit(user.email)
 
-		const message = 'Enviamos um e-mail para confirmar a recuperação de senha'
-		fpPage.noticeShouldBe(message)
+		const message ='Enviamos um e-mail para confirmar a recuperação de senha, verifique sua caixa de entrada.'
+		fpPage.noticeShouldBe(' ')
 	})
 
-	it('deve poder cadastrar uma nova senha', () => {
-		const user = {
-			name: 'Joao Esquecido',
-			email: 'joao@gmail.com',
-			password: 'pwd123',
-			is_shaver: false
-		}
+	context('quando o usuário solicita regaste de senha', () => {
 		
-		
+			const user = {
+				name: 'Will Souza',
+				email: 'will@gmail.com',
+				password: 'pwd123',
+				is_shaver: false
+			}
+	
+			beforeEach(() => {
+				cy.createUser(user)
+				cy.recoveryPass(user.email)
+				cy.getToken(user.email)
+			})
+
+			
+			it('deve poder cadastrar uma nova senha', () => {
+
+			rpPage.go(Cypress.env('passToken'))
+			cy.log(Cypress.env('passToken'))
+	
+		})
 	})
+	
 })
