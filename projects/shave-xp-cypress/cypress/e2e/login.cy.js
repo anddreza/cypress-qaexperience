ae3964e675580ffc39b4a74fdb1cca9dcee36eba
Andreza <andrezasilva744@gmail.com>
2023-04-04 08:36:17 -0300

    Modificações ainda da ula de vlaidações

--- projects/shave-xp-cypress/cypress/e2e/login.cy.js
+++ projects/shave-xp-cypress/cypress/e2e/login.cy.js
@@ -3,8 +3,9 @@
 import loginPage from '../support/pages/login'
 // importando a classe objeto dentro desse login page
 import shaversPage from '../support/pages/shaves'
-import header from '../support/components/header'
-import {sucess, invpass, email404} from '../fixtures/users-login.json' //importando o json dentro do meu teste 
+//import header from '../support/components/header'
+//import {sucess, invpass, email404} from '../fixtures/users-login.json' //importando o json dentro do meu teste 
+import data from '../fixtures/users-login.json'
 
 
 describe('login', () => {
@@ -25,31 +26,32 @@ describe('login', () => {
 
 				// dado que tenho um usuário cadastrado
 				const user = data.sucess 
-				
+				cy.createUser(user)
 				// step que apaga o usuário 
 				// invocar a task que desenvolvemos, o nome dela é removeUser, o que essa massa recebe? user.email, callback then pega o resultado dessa operação no banco de dados 
-				cy.task('removeUser', user.email)
-					.then(function(result){
-						cy.log(result)
-					})
+				// Essa função de remover e criar um novo usuário foi removido daqui e encapsulado dentro de commands.js
+			//	cy.task('removeUser', user.email)
+			//		.then(function(result){
+			//			cy.log(result)
+			//		})
 
 					// Aqui ele está criando um usuário novo, se caso deletar no banco o teste funcionará novamente, tem que automatizar o DELETE 
 					// step que cadastra o usuário SÓ PARA DEPOIS ABRIR A PÁGINA E FAZER O LOGIN COM SUCESSO 
-				cy.request({
-					method: 'POST',
-					url: 'http://localhost:3333/users',
-					body: user //vai mandar a const user que de acordo com a nossa implementação já é a massa de teste no formato perfeito 
-				}).then(function(response){
+			//	cy.request({
+			//		method: 'POST',
+			//		url: 'http://localhost:3333/users',
+			//		body: user //vai mandar a const user que de acordo com a nossa implementação já é a massa de teste no formato perfeito 
+			//	}).then(function(response){
 					// o response vai ser exatamente o que virá da API, será a resposta com o corpo e o status de criado 
-					expect(response.status).to.eq(201)
-				})
+			//		expect(response.status).to.eq(201)
+			//	})
 				// quando faço login com esse usuário
 				loginPage.submit(user.email, user.password)
 				//userShouldLoggenIn(user.name)
 				//loginPage.userShouldBeLoggenIn(user.name) -> é uma validação. 
 
 				// entao sou autenticado com sucesso
-				shaversPage.header.userShouldBeLoggedIn(sucess.name) // chamando a validação que informa o usuário correto na página correta
+				shaversPage.header.userShouldBeLoggedIn(user.name) // chamando a validação que informa o usuário correto na página correta
 
 
 
@@ -99,15 +101,9 @@ describe('login', () => {
 		})
  
 	context('senha muito curta', () => {
-		const passwords = [
-			'1',
-			'12',
-			'123',
-			'1234',
-			'12345'
-		]
-		// data.shortpass.forEach((p) => })
-		passwords.forEach((p) => {
+		
+		 data.shortpass.forEach((p) => {
+		
 			it(`não deve logar com a senha: ${p}`, ()=> {
 				loginPage.submit('andressa@icloud.com', p)
 				loginPage.alertShouldBe('Pelo menos 6 caracteres')
@@ -117,26 +113,11 @@ describe('login', () => {
 	})
 
 	context('email no formato incorreto', () => {
-		
-		const emails = [
-			'andressa&icloud.com',
-			'andressa.com.br',
-			'@icloud.com',
-			'@', 
-			'andressa@',
-			'123123',
-			'!@#!@#',
-			'xpto123'
-		]
-
-		// data.invemails.forEach((e) => {})
-		emails.forEach((e) => {
-			it(`não deve logar com a senha: ${e}`, ()=> {
-				loginPage.submit(e, 'pwd123')
-				loginPage.alertShouldBe('Informe um email válido')
-
-
-			})
-		})
-	})
+        data.invemails.forEach((e) => {
+            it(`não deve logar com o email: ${e}`, () => {
+                loginPage.submit(e, 'pwd123')
+                loginPage.alertShouldBe('Informe um email válido')
+            })
+        })
+    })
 })
