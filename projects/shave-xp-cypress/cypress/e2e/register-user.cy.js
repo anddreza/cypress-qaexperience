456b2acbdd7d6138fe24256626d12d65c633ed40
Andreza <andrezasilva744@gmail.com>
2023-04-10 20:04:51 -0300

    Essa funcionalidade foi copiada diretamente do repositório do shave-xp-cypress, pois na aula ele também já havia buscado de um aluno e agora só irá tratar erros, pontuar coisas

---
+++ projects/shave-xp-cypress/cypress/e2e/register-user.cy.js
@@ -0,0 +1,57 @@
+import data from '../fixtures/users-register.json'
+
+describe('faça seu cadastro', () => {
+    context('quando submeto o formulário', () => {
+        it('deve cadastrar usuário com sucesso', () => {
+            const user = data.success
+
+            cy.deleteUser(user)
+
+            cy.signup(user.name, user.email, user.password)
+
+            const message = 'Boas vindas, faça login para solicitar serviços!'
+            cy.noticeSuccessShouldBe(message)
+        })
+
+        it('não deve recadastrar um email que já existe', () => {
+            const user = data.sameEmail
+
+            cy.createUser(user)
+
+            cy.signup(user.name, user.email, user.password)
+
+            const message = 'Oops! E-mail já cadastrado.'
+            cy.noticeErrorShouldBe(message)
+        })
+
+        it('campos obrigatórios', () => {
+            cy.signup()
+
+            cy.get('.alert-error')
+                .should('have.length', 3)
+                .and(($small) => {
+                    expect($small.get(0).textContent).to.equal('Nome é obrigatório')
+                    expect($small.get(1).textContent).to.equal('E-mail é obrigatório')
+                    expect($small.get(2).textContent).to.equal('Senha é obrigatória')
+                })
+        })
+    })
+
+    context('senha muito curta', () => {
+        data.shortpass.forEach((p) => {
+            it(`não deve cadastrar com a senha: ${p}`, () => {
+                cy.signup('Papito Rocks', 'papito@teste.com.br', p)
+                cy.alertShouldBe('Pelo menos 6 caracteres')
+            })
+        })
+    })
+
+    context('email no formato incorreto', () => {
+        data.invemails.forEach((e) => {
+            it(`não deve cadastrar com o email: ${e}`, () => {
+                cy.signup('Agatha França', e, 'pwd123')
+                cy.alertShouldBe('Informe um email válido')
+            })
+        })
+    })
+})
