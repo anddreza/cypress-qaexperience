40392e04c73f68f48943e75114cee17d0f1c45d7
Andreza <andrezasilva744@gmail.com>
2023-05-26 11:23:11 -0300

    Subindo os comandos realizados

---
+++ projects/shave-xp-cypress/cypress/support/commands/api.js
@@ -0,0 +1,89 @@
+
+Cypress.Commands.add('createUser', (user) => {
+
+
+	//cy.task('removeUser', user.email)
+	//	.then(function (result) {
+	//		cy.log(result)
+	//	})
+
+	// verificar o que está vindo 
+	cy.log(JSON.stringify(user))
+	//cy.request({
+	//	method: 'DELETE',
+	//	url: 'http://localhost:5000/user/' + user.email
+	//}).then(function (response) {
+	//	expect(response.status).to.eq(204)
+	//})
+
+	cy.request({
+		method: 'POST',
+		url: Cypress.env('apiHelper') + '/user',
+		body: user
+	}).then(function (response) {
+		expect(response.status).to.eq(201)
+	})
+})
+//cy.request({
+//	method: 'POST',
+//	url: 'http://localhost:5000/user',
+//	body: user
+//	}).then(function (response) {
+//		expect(response.status).to.eq(201)
+//	})
+
+//})
+
+Cypress.Commands.add('deleteUser', (user) => {
+
+	cy.request({
+		method: 'DELETE',
+		url: Cypress.env('apiHelper') + '/user' + user.email
+	}).then(function (response) {
+		expect(response.status).to.eq(204)
+	})
+
+})
+
+Cypress.Commands.add('recoveryPass', (email) => {
+	cy.request({
+		method: 'POST',
+		url: 'http://localhost:3333/password/forgot',
+		body: { email: email }
+	}).then(result => {
+		expect(result.status).to.eql(204)
+	})
+})
+
+Cypress.Commands.add('getToken', (email) => {
+	cy.request({
+		method: 'GET',
+		url: 'http://localhost:5000/token' + email
+	}).then(result => {
+		expect(result.status).to.eql(200)
+		cy.log(result.body.token)
+		Cypress.env('passToken', result.body.token)
+	})
+
+})
+
+
+
+Cypress.Commands.add('apiLogin', (user) => {
+	cy.request({
+		method: 'POST',
+		url: 'http://localhost:3333/sessions',
+		body: { email: user.email, password: user.password }
+	}).then(response => {
+		expect(response.status).to.eql(200)
+
+		const { user, token } = response.body
+
+		window.localStorage.setItem('@ShaveXP:token', token)
+		window.localStorage.setItem('@ShaveXP:user', JSON.stringify(user))
+
+	})
+
+	cy.visit('/')
+
+})
